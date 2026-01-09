f562c888b80a581fd7f067e89a6f17f766f38547
Ayaansoftdev007 <ayaan007ayk@gmail.com>
2026-01-10 04:16:15 +0530

    Fix: Removed duplicate generateRandomString in user.js causing backend crash

--- backend/routes/admin/user.js
+++ backend/routes/admin/user.js
@@ -180,13 +180,3 @@ module.exports = app => {
         }
     });
 }
-
-function generateRandomString(length) {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz^#@&$0123456789';
-    let result = '';
-    for (let i = 0; i < length; i++) {
-        const randomIndex = Math.floor(Math.random() * characters.length);
-        result += characters.charAt(randomIndex);
-    }
-    return result;
-}
