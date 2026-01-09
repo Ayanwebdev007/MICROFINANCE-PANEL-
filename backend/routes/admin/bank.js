ff09cb0d440cd4fdf252b52484ebc6d1d32e7969
Ayaansoftdev007 <ayaan007ayk@gmail.com>
2026-01-10 04:09:52 +0530

    Fix: Removed duplicate generateRandomString declaration in bank.js.

--- backend/routes/admin/bank.js
+++ backend/routes/admin/bank.js
@@ -260,14 +260,3 @@ module.exports = app => {
         }
     });
 }
-
-
-function generateRandomString(length) {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-    let result = '';
-    for (let i = 0; i < length; i++) {
-        const randomIndex = Math.floor(Math.random() * characters.length);
-        result += characters.charAt(randomIndex);
-    }
-    return result;
-}
